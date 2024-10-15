12f878b21b8e4ebfa10040d6ca482364beff64f9
Christine Matthews <candreola@gmail.com>
2024-10-15 13:28:32 -0500

    add resume

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -2,6 +2,71 @@ export default function Resume () {
     return (
         <div className="content">
             <h2>Resume</h2>
+            <hr></hr>
+            <div className="resume">
+                <div className="work">
+                    <h3>Work Experience</h3>
+                    <h4>Technical Product Analyst</h4>
+                    <h5>May 2023 - Present</h5>
+                    <h6>Moody Bible Institute | ITS Department - Web Technologies</h6>
+                    <ul>
+                        <li>Manage CSO by ensuring that any bugs and enhancement requests for our web products get developed, are thouroughly tested to ensure that they work in the test environments, and get deployed to production on time.</li>
+                        <li>Design new blocks needed for larger enhancements needed.</li>
+                        <li>Write detailed test plans for major website replatforms to ensure each page, block, and button works as expected for both the user experience and content managers.</li>
+                    </ul>
+                    <h4>Technical Analyst</h4>
+                    <h5>May 2022 - May 2023</h5>
+                    <h6>Moody Bible Institute | ITS Department - Web Technologies</h6>
+                    <ul>
+                        <li>Work under the Technical Product Analyst to ensure any testing during deployments has a second pair of eyes.</li>
+                    </ul>
+                    <h4>Technical Support Specialist</h4>
+                    <h5>May 2021 - May 2022</h5>
+                    <h6>Moody Bible Institute | ITS Department - Support Center</h6>
+                    <ul>
+                        <li>Ensure customer issues regarding accounts, web pages, and hardware are solved by troubleshooting issues and finding solutions for each individual problem</li>
+                        <li>Create onboarding training videos for staff and students by recording content, designing animations, and editing.</li>
+                    </ul>
+                </div>
+                <div className="edu">
+                    <h3>Education</h3>
+                    <h4>Full-Stack Software Developer Certificate</h4>
+                    <h6>Northwestern University</h6>
+                    <p><strong>Skills include:</strong></p>
+                    <ul>
+                        <li>Html</li>
+                        <li>CSS</li>
+                        <li>CSS Frameworks</li>
+                        <li>Javascript</li>
+                        <li>JQuery</li>
+                        <li>Node.js</li>
+                        <li>Express.js</li>
+                        <li>SQL</li>
+                        <li>Mongoose</li>
+                        <li>React</li>
+                        <li>GraphQL</li>
+                    </ul>
+                    <h4>BA in Communications - Non-Profit Marketing Communications</h4>
+                    <h6>Moody Bible Institute</h6>
+                    <p><strong>Skills include:</strong></p>
+                    <ul>
+                        <li>Media Strategies</li>
+                        <li>Graphic Design</li>
+                        <li>Non-Profit Marketing Strategies</li>
+                        <li>Videography</li>
+                    </ul>
+                </div>
+                <div className="contact">
+                    <h4>Contact</h4>
+                    <ul>
+                        <li><span>Email:</span> <a href="mailto:matthews.christinemarie@gmail.com">matthews.christinemarie@gmail.com</a></li>
+                        <li><span>Phone:</span><a href="phone:6089212357">(608) 921-2357</a></li>
+                        <li><span>Github:</span><a href="https://github.com/cmatthews3212">www.github.com/cmatthews3212</a></li>
+                        <li><span>LinkedIn:</span><a href="https://www.linkedin.com/in/christine-andreola-matthews-21a7241b5/">www.linkedin.com/in/christine-andreola-matthews-21a7241b5/</a></li>
+                    </ul>
+                </div>
+
+            </div>
         </div>
     )
 }
