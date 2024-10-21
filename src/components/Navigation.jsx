9120ec211c248f76796421c6bfa8e4ccc660e160
Christine Matthews <candreola@gmail.com>
2024-10-21 14:06:30 -0500

    make menu items viisble on desktop

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react';
 
 const Navigation = ({ currentPage, handlePageChange}) => {
-    const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = window.innerWidth < 850 ? useState(false) : useState(true)
 
     const handleMenuClick = () => {
-        setIsVisible(prevVisible => !prevVisible);
+        if (window.innerWidth < 850) {
+            setIsVisible(prevVisible => !prevVisible);
+
+        } else {
+            setIsVisible(true)
+        }
     }
 
     const handleClick = (page, event) => {
         event.preventDefault();
+       if (window.innerWidth < 850) {
         setIsVisible(false)
+       }
         handlePageChange(page);
     };
 
@@ -24,7 +31,7 @@ const Navigation = ({ currentPage, handlePageChange}) => {
                 <div className="hamIcon"></div>
                 <div className="hamIcon"></div>
             </div>
-       <ul style={{ display: isVisible ? 'block' : 'none'}}>
+       <ul className='menu-items' style={{ display: isVisible ? 'block' : 'none'}}>
             <li className={currentPage === 'About' ? ' active' : ''}>
                 <a 
                 href="#about" 
